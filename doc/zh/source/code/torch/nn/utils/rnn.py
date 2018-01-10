a75b9d0d82c4874e10ff757a09d747f7a74eff0c
wangyangting <yting_xmei1129@163.com>
2018-01-10 15:49:27 +0800

    add torch & torchvision source code

---
+++ doc/zh/source/code/torch/nn/utils/rnn.py
@@ -0,0 +1,128 @@
+from collections import namedtuple
+import torch
+from torch.autograd import Variable
+
+
+PackedSequence_ = namedtuple('PackedSequence', ['data', 'batch_sizes'])
+
+
+class PackedSequence(PackedSequence_):
+    r"""Holds the data and list of batch_sizes of a packed sequence.
+
+    All RNN modules accept packed sequences as inputs.
+
+    Note:
+        Instances of this class should never be created manually. They are meant
+        to be instantiated by functions like :func:`pack_padded_sequence`.
+
+    Attributes:
+        data (Variable): Variable containing packed sequence
+        batch_sizes (list[int]): list of integers holding information about
+            the batch size at each sequence step
+    """
+    pass
+
+
+def pack_padded_sequence(input, lengths, batch_first=False):
+    r"""Packs a Variable containing padded sequences of variable length.
+
+    Input can be of size ``TxBx*`` where T is the length of the longest sequence
+    (equal to ``lengths[0]``), B is the batch size, and * is any number of
+    dimensions (including 0). If ``batch_first`` is True ``BxTx*`` inputs are
+    expected.
+
+    The sequences should be sorted by length in a decreasing order, i.e.
+    ``input[:,0]`` should be the longest sequence, and ``input[:,B-1]`` the
+    shortest one.
+
+    Note:
+        This function accept any input that has at least two dimensions. You
+        can apply it to pack the labels, and use the output of the RNN with
+        them to compute the loss directly. A Variable can be retrieved from
+        a :class:`PackedSequence` object by accessing its ``.data`` attribute.
+
+    Arguments:
+        input (Variable): padded batch of variable length sequences.
+        lengths (list[int]): list of sequences lengths of each batch element.
+        batch_first (bool, optional): if ``True``, the input is expected in BxTx*
+            format.
+
+    Returns:
+        a :class:`PackedSequence` object
+    """
+    if lengths[-1] <= 0:
+        raise ValueError("length of all samples has to be greater than 0, "
+                         "but found an element in 'lengths' that is <=0")
+    if batch_first:
+        input = input.transpose(0, 1)
+
+    steps = []
+    batch_sizes = []
+    lengths_iter = reversed(lengths)
+    batch_size = input.size(1)
+    if len(lengths) != batch_size:
+        raise ValueError("lengths array has incorrect size")
+
+    prev_l = 0
+    for i, l in enumerate(lengths_iter):
+        if l > prev_l:
+            c_batch_size = batch_size - i
+            steps.append(input[prev_l:l, :c_batch_size].contiguous().view(-1, *input.size()[2:]))
+            batch_sizes.extend([c_batch_size] * (l - prev_l))
+            prev_l = l
+        elif prev_l > l:  # remember that new_length is the preceding length in the array
+            raise ValueError("lengths array has to be sorted in decreasing order")
+
+    return PackedSequence(torch.cat(steps), batch_sizes)
+
+
+def pad_packed_sequence(sequence, batch_first=False, padding_value=0.0):
+    r"""Pads a packed batch of variable length sequences.
+
+    It is an inverse operation to :func:`pack_padded_sequence`.
+
+    The returned Variable's data will be of size TxBx*, where T is the length
+    of the longest sequence and B is the batch size. If ``batch_first`` is True,
+    the data will be transposed into BxTx* format.
+
+    Batch elements will be ordered decreasingly by their length.
+
+    Arguments:
+        sequence (PackedSequence): batch to pad
+        batch_first (bool, optional): if ``True``, the output will be in BxTx*
+            format.
+        padding_value (float, optional): values for padded elements
+
+    Returns:
+        Tuple of Variable containing the padded sequence, and a list of lengths
+        of each sequence in the batch.
+    """
+    var_data, batch_sizes = sequence
+    max_batch_size = batch_sizes[0]
+    output = var_data.data.new(len(batch_sizes), max_batch_size, *var_data.size()[1:]).fill_(padding_value)
+    output = Variable(output)
+
+    lengths = []
+    data_offset = 0
+    prev_batch_size = batch_sizes[0]
+    prev_i = 0
+    for i, batch_size in enumerate(batch_sizes):
+        if batch_size != prev_batch_size:
+            l = prev_batch_size * (i - prev_i)
+            output[prev_i:i, :prev_batch_size] = var_data[data_offset:data_offset + l]
+            data_offset += l
+            prev_i = i
+        dec = prev_batch_size - batch_size
+        if dec > 0:
+            lengths.extend((i,) * dec)
+        prev_batch_size = batch_size
+
+    l = prev_batch_size * (len(batch_sizes) - prev_i)
+    output[prev_i:, :prev_batch_size] = var_data[data_offset:data_offset + l]
+
+    lengths.extend((i + 1,) * batch_size)
+    lengths.reverse()
+
+    if batch_first:
+        output = output.transpose(0, 1)
+    return output, lengths
---
+++ doc/zh/source/code/torchvision/datasets/lsun.py
@@ -0,0 +1,146 @@
+import torch.utils.data as data
+from PIL import Image
+import os
+import os.path
+import six
+import string
+import sys
+if sys.version_info[0] == 2:
+    import cPickle as pickle
+else:
+    import pickle
+
+
+class LSUNClass(data.Dataset):
+    def __init__(self, db_path, transform=None, target_transform=None):
+        import lmdb
+        self.db_path = db_path
+        self.env = lmdb.open(db_path, max_readers=1, readonly=True, lock=False,
+                             readahead=False, meminit=False)
+        with self.env.begin(write=False) as txn:
+            self.length = txn.stat()['entries']
+        cache_file = '_cache_' + db_path.replace('/', '_')
+        if os.path.isfile(cache_file):
+            self.keys = pickle.load(open(cache_file, "rb"))
+        else:
+            with self.env.begin(write=False) as txn:
+                self.keys = [key for key, _ in txn.cursor()]
+            pickle.dump(self.keys, open(cache_file, "wb"))
+        self.transform = transform
+        self.target_transform = target_transform
+
+    def __getitem__(self, index):
+        img, target = None, None
+        env = self.env
+        with env.begin(write=False) as txn:
+            imgbuf = txn.get(self.keys[index])
+
+        buf = six.BytesIO()
+        buf.write(imgbuf)
+        buf.seek(0)
+        img = Image.open(buf).convert('RGB')
+
+        if self.transform is not None:
+            img = self.transform(img)
+
+        if self.target_transform is not None:
+            target = self.target_transform(target)
+
+        return img, target
+
+    def __len__(self):
+        return self.length
+
+    def __repr__(self):
+        return self.__class__.__name__ + ' (' + self.db_path + ')'
+
+
+class LSUN(data.Dataset):
+    """
+    `LSUN <http://lsun.cs.princeton.edu>`_ dataset.
+
+    Args:
+        db_path (string): Root directory for the database files.
+        classes (string or list): One of {'train', 'val', 'test'} or a list of
+            categories to load. e,g. ['bedroom_train', 'church_train'].
+        transform (callable, optional): A function/transform that  takes in an PIL image
+            and returns a transformed version. E.g, ``transforms.RandomCrop``
+        target_transform (callable, optional): A function/transform that takes in the
+            target and transforms it.
+    """
+
+    def __init__(self, db_path, classes='train',
+                 transform=None, target_transform=None):
+        categories = ['bedroom', 'bridge', 'church_outdoor', 'classroom',
+                      'conference_room', 'dining_room', 'kitchen',
+                      'living_room', 'restaurant', 'tower']
+        dset_opts = ['train', 'val', 'test']
+        self.db_path = db_path
+        if type(classes) == str and classes in dset_opts:
+            if classes == 'test':
+                classes = [classes]
+            else:
+                classes = [c + '_' + classes for c in categories]
+        if type(classes) == list:
+            for c in classes:
+                c_short = c.split('_')
+                c_short.pop(len(c_short) - 1)
+                c_short = '_'.join(c_short)
+                if c_short not in categories:
+                    raise(ValueError('Unknown LSUN class: ' + c_short + '.'
+                                     'Options are: ' + str(categories)))
+                c_short = c.split('_')
+                c_short = c_short.pop(len(c_short) - 1)
+                if c_short not in dset_opts:
+                    raise(ValueError('Unknown postfix: ' + c_short + '.'
+                                     'Options are: ' + str(dset_opts)))
+        else:
+            raise(ValueError('Unknown option for classes'))
+        self.classes = classes
+
+        # for each class, create an LSUNClassDataset
+        self.dbs = []
+        for c in self.classes:
+            self.dbs.append(LSUNClass(
+                db_path=db_path + '/' + c + '_lmdb',
+                transform=transform))
+
+        self.indices = []
+        count = 0
+        for db in self.dbs:
+            count += len(db)
+            self.indices.append(count)
+
+        self.length = count
+        self.target_transform = target_transform
+
+    def __getitem__(self, index):
+        """
+        Args:
+            index (int): Index
+
+        Returns:
+            tuple: Tuple (image, target) where target is the index of the target category.
+        """
+        target = 0
+        sub = 0
+        for ind in self.indices:
+            if index < ind:
+                break
+            target += 1
+            sub = ind
+
+        db = self.dbs[target]
+        index = index - sub
+
+        if self.target_transform is not None:
+            target = self.target_transform(target)
+
+        img, _ = db[index]
+        return img, target
+
+    def __len__(self):
+        return self.length
+
+    def __repr__(self):
+        return self.__class__.__name__ + ' (' + self.db_path + ')'
