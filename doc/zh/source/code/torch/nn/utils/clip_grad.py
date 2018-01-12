0a48201c105c09f8722dc16fb2284a1daa89cc0e
Eadral <a250014238@me.com>
2018-01-12 18:38:52 +0800

    translated: clip_grad_norm

--- doc/zh/source/code/torch/nn/utils/clip_grad.py
+++ doc/zh/source/code/torch/nn/utils/clip_grad.py
@@ -1,6 +1,6 @@
 
 def clip_grad_norm(parameters, max_norm, norm_type=2):
-    r"""对参数的梯度按范数进行裁剪.
+    r"""接收一个包含Variable的可迭代对象, 对Variable的梯度按范数进行裁剪.
 
     范数是对所有梯度进行计算的, 等价于把所有输入变量的梯度连接成一个向量, 对这个向量按范数进行裁剪. 梯度将会被原地修改.
 
