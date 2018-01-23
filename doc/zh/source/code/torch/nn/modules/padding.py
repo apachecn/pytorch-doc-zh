9a559655fe375c12d1ba7dc3a12b15c0b30880a3
噼里啪啦嘣 <zybourn@foxmail.com>
2018-01-23 21:43:13 +0800

    完成

--- doc/zh/source/code/torch/nn/modules/padding.py
+++ doc/zh/source/code/torch/nn/modules/padding.py
@@ -43,26 +43,26 @@ class ConstantPad1d(Module):
 
 
 class ConstantPad2d(Module):
-    r"""Pads the input tensor boundaries with a constant value.
+    r"""用一个常数值填充输入张量边界.
 
-    For Nd-padding, use nn.functional.pad().
+    对于 Nd-padding, 使用 nn.functional.pad().
 
-    Args:
-        padding (int, tuple): the size of the padding. If is int, uses the same
-            padding in all boundaries. If a 4-tuple, uses (paddingLeft, paddingRight, paddingTop, paddingBottom)
+    参数:
+        padding (int, tuple):填充的大小。 如果是int，则在所有边界使用相同的填充。
+             如果是4个元组，使用 (paddingLeft, paddingRight, paddingTop, paddingBottom)
 
-    Shape:
+    形态:
         - Input: :math:`(N, C, H_{in}, W_{in})`
         - Output: :math:`(N, C, H_{out}, W_{out})` where
           :math:`H_{out} = H_{in} + paddingTop + paddingBottom`
           :math:`W_{out} = W_{in} + paddingLeft + paddingRight`
 
-    Examples::
+    实例::
 
         >>> m = nn.ConstantPad2d(3, 3.5)
         >>> input = autograd.Variable(torch.randn(16, 3, 320, 480))
         >>> output = m(input)
-        >>> # using different paddings
+        >>> # 使用不同的填充
         >>> m = nn.ConstantPad2d((3, 3, 6, 6), 3.5)
         >>> output = m(input)
 
@@ -160,7 +160,7 @@ class ReflectionPad2d(Module):
 
     参数:
         padding (int, tuple): 填充的大小。 如果是int，则在所有边界填充使用相同的.
-        如果是4-tuple，则使用 (paddingLeft, paddingRight, paddingTop, paddingBottom)
+        如果是4个元组，则使用 (paddingLeft, paddingRight, paddingTop, paddingBottom)
         
     形态:
         - Input: :math:`(N, C, H_{in}, W_{in})`
@@ -173,7 +173,7 @@ class ReflectionPad2d(Module):
         >>> m = nn.ReflectionPad2d(3)
         >>> input = autograd.Variable(torch.randn(16, 3, 320, 480))
         >>> output = m(input)
-        >>> # using different paddings
+        >>> # 使用不同的填充
         >>> m = nn.ReflectionPad2d((3, 3, 6, 6))
         >>> output = m(input)
 
@@ -192,23 +192,23 @@ class ReflectionPad2d(Module):
 
 
 class ReplicationPad1d(Module):
-    r"""Pads the input tensor using replication of the input boundary.
+    r"""使用输入边界的复制填充输入张量.
 
-    Args:
-        padding (int, tuple): the size of the padding. If is int, uses the same
-            padding in all boundaries. If a 2-tuple, uses (paddingLeft, paddingRight)
-
-    Shape:
+    参数:
+    padding (int, tuple): 填充的大小。 如果是int，则在所有边界使用相同的填充。
+             如果一个2元组，使用 (paddingLeft, paddingRight)
+        
+    形态:
         - Input: :math:`(N, C, W_{in})`
         - Output: :math:`(N, C, W_{out})` where
           :math:`W_{out} = W_{in} + paddingLeft + paddingRight`
 
-    Examples::
+    实例::
 
         >>> m = nn.ReplicationPad1d(3)
         >>> input = autograd.Variable(torch.randn(16, 3, 480))
         >>> output = m(input)
-        >>> # using different paddings
+        >>> # 使用不同的填充
         >>> m = nn.ReplicationPad1d((3, 6))
         >>> output = m(input)
 
@@ -227,24 +227,24 @@ class ReplicationPad1d(Module):
 
 
 class ReplicationPad2d(Module):
-    r"""Pads the input tensor using replication of the input boundary.
+    r"""使用输入边界的复制填充输入张量.
 
-    Args:
-        padding (int, tuple): the size of the padding. If is int, uses the same
-            padding in all boundaries. If a 4-tuple, uses (paddingLeft, paddingRight, paddingTop, paddingBottom)
+    参数:
+        padding (int, tuple): 填充的大小. 如果是int，则在所有边界使用相同的填充.
+         如果是4个元组，则使用(paddingLeft, paddingRight, paddingTop, paddingBottom)
 
-    Shape:
+    形态:
         - Input: :math:`(N, C, H_{in}, W_{in})`
         - Output: :math:`(N, C, H_{out}, W_{out})` where
           :math:`H_{out} = H_{in} + paddingTop + paddingBottom`
           :math:`W_{out} = W_{in} + paddingLeft + paddingRight`
 
-    Examples::
+    实例::
 
         >>> m = nn.ReplicationPad2d(3)
         >>> input = autograd.Variable(torch.randn(16, 3, 320, 480))
         >>> output = m(input)
-        >>> # using different paddings
+        >>> # 使用不同的填充
         >>> m = nn.ReplicationPad2d((3, 3, 6, 6))
         >>> output = m(input)
 
@@ -263,26 +263,26 @@ class ReplicationPad2d(Module):
 
 
 class ReplicationPad3d(Module):
-    r"""Pads the input tensor using replication of the input boundary.
+    r"""使用输入边界的复制填充输入张量.
 
-    Args:
-        padding (int, tuple): the size of the padding. If is int, uses the same
-            padding in all boundaries. If a 6-tuple, uses (paddingLeft, paddingRight,
+    参数:
+        padding (int, tuple): 填充的大小. 如果是int，则在所有边界使用相同的填充.
+        . 如果是四个元组, 则使用 (paddingLeft, paddingRight,
             paddingTop, paddingBottom, paddingFront, paddingBack)
 
-    Shape:
+    形态:
         - Input: :math:`(N, C, D_{in}, H_{in}, W_{in})`
         - Output: :math:`(N, C, D_{out}, H_{out}, W_{out})` where
           :math:`D_{out} = D_{in} + paddingFront + paddingBack`
           :math:`H_{out} = H_{in} + paddingTop + paddingBottom`
           :math:`W_{out} = W_{in} + paddingLeft + paddingRight`
 
-    Examples::
+    实例::
 
         >>> m = nn.ReplicationPad3d(3)
         >>> input = autograd.Variable(torch.randn(16, 3, 8, 320, 480))
         >>> output = m(input)
-        >>> # using different paddings
+        >>> # 使用不同的填充
         >>> m = nn.ReplicationPad3d((3, 3, 6, 6, 1, 1))
         >>> output = m(input)
 
@@ -301,24 +301,24 @@ class ReplicationPad3d(Module):
 
 
 class ZeroPad2d(ConstantPad2d):
-    r"""Pads the input tensor boundaries with zero.
+    r"""用零填充输入张量边界.
 
-    Args:
-        padding (int, tuple): the size of the padding. If is int, uses the same
-            padding in all boundaries. If a 4-tuple, uses (paddingLeft, paddingRight, paddingTop, paddingBottom)
+    参数:
+        padding (int, tuple): 填充的大小. 如果是int，则在所有边界使用相同的填充.
+        . 如果是四个元组, 则使用 (paddingLeft, paddingRight, paddingTop, paddingBottom)
 
-    Shape:
+    形态:
         - Input: :math:`(N, C, H_{in}, W_{in})`
         - Output: :math:`(N, C, H_{out}, W_{out})` where
           :math:`H_{out} = H_{in} + paddingTop + paddingBottom`
           :math:`W_{out} = W_{in} + paddingLeft + paddingRight`
 
-    Examples::
+    实例::
 
         >>> m = nn.ZeroPad2d(3)
         >>> input = autograd.Variable(torch.randn(16, 3, 320, 480))
         >>> output = m(input)
-        >>> # using different paddings
+        >>> # 使用不同的填充
         >>> m = nn.ZeroPad2d((3, 3, 6, 6))
         >>> output = m(input)
 
