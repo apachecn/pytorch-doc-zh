56ebd2a67f2746669797e4ceb20394cdc31d4583
Yuhai Li <yuhaili@Yuhais-MacBook-Pro.local>
2018-01-24 13:14:41 -0800

    multilabel_margin_loss

--- doc/zh/source/code/torch/nn/functional.py
+++ doc/zh/source/code/torch/nn/functional.py
@@ -1168,9 +1168,7 @@ def hinge_embedding_loss(input, target, margin=1.0, size_average=True):
 
 
 multilabel_margin_loss = _add_docstr(torch._C._nn.multilabel_margin_loss, r"""
-multilabel_margin_loss(input, target, size_average=True) -> Variable
-
-See :class:`~torch.nn.MultiLabelMarginLoss` for details.
+详见 :class:`~torch.nn.MultiLabelMarginLoss`.
 """)
 
 soft_margin_loss = _add_docstr(torch._C._nn.soft_margin_loss, r"""
