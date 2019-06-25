695cf83041d0b5edfb75df3c868dde66d0a94109
jiangzhonglian <jiang-s@163.com>
2019-06-25 15:25:35 +0800

    注释多余代码

--- run_website.sh
+++ run_website.sh
@@ -5,8 +5,8 @@ logerror() { echo "[ERROR] $@" 1>&2; }
 python3 src/script.py "home" "book"
 rm -rf node_modules/gitbook-plugin-tbfed-pagefooter
 gitbook install
-python3 src/script.py "home" "powered"
-python3 src/script.py "home" "gitalk"
+# python3 src/script.py "home" "powered"
+# python3 src/script.py "home" "gitalk"
 gitbook build ./ _book
 # python3 src/script.py "home" "index"
 
@@ -28,11 +28,11 @@ for version in $versions;do
     echo "gitbook install docs/${version}"
     gitbook install docs/${version}
 
-    echo "python3 src/script.py ${version} powered"
-    python3 src/script.py ${version} "powered"
+    # echo "python3 src/script.py ${version} powered"
+    # python3 src/script.py ${version} "powered"
 
-    echo "python3 src/script.py ${version} gitalk"
-    python3 src/script.py ${version} "gitalk"
+    # echo "python3 src/script.py ${version} gitalk"
+    # python3 src/script.py ${version} "gitalk"
 
     echo "gitbook build docs/${version} _book/docs/${version}"
     gitbook build docs/${version} _book/docs/${version}
